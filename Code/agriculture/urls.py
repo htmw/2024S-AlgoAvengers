785029c950783e7f0b2a608f13d78eeb56aade40
LOKESH538 <69184455+LOKESH538@users.noreply.github.com>
2024-04-29 17:51:29 -0400

    Updated code on  urls.py file

--- Code/agriculture/urls.py
+++ Code/agriculture/urls.py
@@ -17,20 +17,24 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from main_app.views import area_details, index, training_model, weather_forecasting
+from main_app.views import area_details, register, user_login, user_logout, index, training_model, weather_forecasting, history
 
 from main_app.views import about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
     path('about/', about),
     path('area-details/', area_details),
     path('weather-forecasting/', weather_forecasting),
     path('training-model/', training_model),
+    path('history/', history, name='history'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-admin.site.site_header  =  "FarmEasy admin"  
-admin.site.site_title  =  "FarmEasy admin site"
-admin.site.index_title  =  "FarmEasy Admin"
+admin.site.site_header  =  "Ecoscan admin"  
+admin.site.site_title  =  "Ecoscan admin site"
+admin.site.index_title  =  "Ecoscan Admin"
