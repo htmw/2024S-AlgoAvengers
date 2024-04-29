09d118823bfe23ed0a882fd1592e61a4fe9da3d4
LOKESH538 <69184455+LOKESH538@users.noreply.github.com>
2024-04-29 18:33:26 -0400

    Updated models.py file

--- Code/main_app/models.py
+++ Code/main_app/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class LeafDiseaseHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.TextField()
+    disease_name = models.CharField(max_length=100)
+    description = models.TextField()
+    symptoms = models.TextField()
+    prevent = models.TextField()
+    supplement_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.disease_name}"
+
 
 class Areas(models.Model):
     name = models.CharField(max_length=50, blank=True, null=True)
